05629ce68f64c6eda3e61122928785a6f40d4c99
ThinhLD <thinhld@telsoft.com.vn>
2024-05-03 09:47:46 +0700

    init

---
+++ src/entity/Student.java
@@ -0,0 +1,10 @@
+package entity;
+
+public class Student extends Person{
+    public Student(String s) {
+        setName(s);
+    }
+    public Student(String s,String s1) {
+        setName(s);
+    }
+}
